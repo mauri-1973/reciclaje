1da3586a8ccafb62d7129cc468df766247f3cc21
mauri-1973 <mauri-1973@outlook.cl>
2024-12-14 06:36:07 -0300

    Guardando cambios olvidados

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,5 +1,6 @@
 class Delivery < ApplicationRecord
   belongs_to :material
   belongs_to :recycler
+  validates :material_id, presence: true
   validates :delivery_date, :quantity, presence: true
 end
